780c3b9011777cb96b93ef89ca489138901bfb18
saberlkh <347827409@qq.com>
2024-02-29 22:59:38 +0800

    [add]:添加edu-book-service服务

---
+++ edu-book-service/src/main/kotlin/com/edu/book/EduBoolServiceApplication.java
@@ -0,0 +1,13 @@
+package com.edu.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduBoolServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduBoolServiceApplication.class, args);
+    }
+
+}
