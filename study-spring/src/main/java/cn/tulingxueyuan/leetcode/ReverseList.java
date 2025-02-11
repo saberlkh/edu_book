5b930dfbf4f22cc59bcae77336fb8d1a0aaa8ac9
liukaihua <347827409@qq.com>
2025-02-11 15:21:27 +0800

    [fix]:study file commit

--- study-spring/src/main/java/cn/tulingxueyuan/leetcode/ReverseList.java
+++ study-spring/src/main/java/cn/tulingxueyuan/leetcode/ReverseList.java
@@ -26,12 +26,27 @@ public class ReverseList {
         return pre;
     }
 
+    /**
+     * 递归
+     * @param head
+     * @return
+     */
+    public static ListNode recursion(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = recursion(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
     public static void main(String[] args) {
         ListNode head = new ListNode(null, 1);
         ListNode head2 = new ListNode(head, 2);
         ListNode head3 = new ListNode(head2, 3);
         ListNode head4 = new ListNode(head3, 4);
-        ListNode prev = iterate(head4);
+        ListNode prev = recursion(head4);
         System.out.println(JSON.toJSONString(prev));
     }
 
