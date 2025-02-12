564679dbeb4a23a9935cf9ae5767bff8e7583335
liukaihua <347827409@qq.com>
2025-02-12 15:55:07 +0800

    [fix]:study file commit

--- study-spring/src/main/java/cn/tulingxueyuan/leetcode/Solution.java
+++ study-spring/src/main/java/cn/tulingxueyuan/leetcode/Solution.java
@@ -16,6 +16,8 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println(JSON.toJSONString(solution1(new int[]{1, 2, 3, 4, 5, 6}, 10)));
         System.out.println(JSON.toJSONString(solution(new int[]{1, 2, 3, 4, 5, 6}, 10)));
+        System.out.println(JSON.toJSONString(twoSearch(new int[]{1, 2, 3, 4, 5, 6}, 10)));
+        System.out.println(JSON.toJSONString(twoPoint(new int[]{1, 2, 3, 4, 5, 6}, 10)));
     }
 
     /**
@@ -42,6 +44,50 @@ public class Solution {
                 return new int[]{map.get(target - nums[i]), i};
             }
             map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    /**
+     * 二分查找
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSearch(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            int low = i, high = nums.length - 1;
+            while (low <= high) {
+                int mid = (high - low) / 2 + low;
+                if (nums[mid] == target - nums[i]) {
+                    return new int[]{i, mid};
+                } else if (nums[mid] > target - nums[i]) {
+                    high = mid - 1;
+                } else  {
+                    low = mid + 1;
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+    /**
+     * 双指针
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoPoint(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int sum = nums[low] + nums[high];
+            if (sum == target) {
+                return new int[]{low, high};
+            } else if (sum < target) {
+                low++;
+            } else {
+                high--;
+            }
         }
         return new int[]{};
     }
