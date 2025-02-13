63c236c14765d180f320b349cd999055c4cec060
liukaihua <347827409@qq.com>
2025-02-13 16:30:41 +0800

    [fix]:study file commit

--- study-spring/src/main/java/cn/tulingxueyuan/leetcode/MergeSortArray.java
+++ study-spring/src/main/java/cn/tulingxueyuan/leetcode/MergeSortArray.java
@@ -14,7 +14,7 @@ public class MergeSortArray {
         int[] nums1 = {1, 3, 5, 7, 9, 0, 0, 0, 0};
         int[] nums2 = {2, 4, 6, 8};
 //        System.out.println(Arrays.toString(merge(nums1, 5, nums2, 4)));
-        System.out.println(Arrays.toString(merge2(nums1, 5, nums2, 4)));
+        System.out.println(Arrays.toString(merge3(nums1, 5, nums2, 4)));
     }
 
     public static int[] merge(int[] nums1, int m, int[] nums2, int n) {
@@ -38,6 +38,17 @@ public class MergeSortArray {
         if (p2 < n) {
             System.arraycopy(nums2, p2, nums1, p1 + p2, m + n - p1 - p2);
         }
+        return nums1;
+    }
+
+    public static int[] merge3(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            nums1[p--] = nums1[p1] > nums2[p2] ? nums1[p1--] : nums2[p2--];
+        }
+        System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
         return nums1;
     }
 
