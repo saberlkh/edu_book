fdebf0b310140da3779851831de85525bf2c4413
liukaihua <347827409@qq.com>
2025-02-14 14:52:13 +0800

    [fix]:study file commit

---
+++ study-spring/src/main/java/cn/tulingxueyuan/leetcode/Triangles.java
@@ -0,0 +1,27 @@
+package cn.tulingxueyuan.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @Classname Triangles
+ * @Description TODO
+ * @Date 2025/2/14 14:47
+ * @Created by liukaihua
+ */
+public class Triangles {
+
+    public static void main(String[] args) {
+        System.out.println(largestPerimeter(new int[]{3, 6, 2, 3}));
+    }
+
+    public static int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = nums.length - 1; i >= 2; i--) {
+            if (nums[i - 1] + nums[i - 2] > nums[i]) {
+                return nums[i - 1] + nums[i -2 ] + nums[i];
+            }
+        }
+        return 0;
+    }
+
+}
